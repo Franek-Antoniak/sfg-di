626bfa4590c415516240d02c4224837785f9e179
Franek Antoniak <franekant123@gmail.com>
2022-09-23 00:01:24 +0200

    build: clean spring boot project

---
+++ src/test/java/frank/antoniak/sfgdi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package frank.antoniak.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
